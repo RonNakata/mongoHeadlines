d5a754f5a89d2f511eed35cadc14f51cd27bdbdb
RonNakata <ronald_nakata@msn.com>
2018-08-09 15:49:20 -0400

    env

--- controllers/routes.js
+++ controllers/routes.js
@@ -27,7 +27,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoheadlines");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
